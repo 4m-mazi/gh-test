ba58f32cc6e05d5da880f078dce105b7c92ad05d
Mogyuchi <git@mogyuchi.com>
2022-05-04 15:12:18 +0900

    config test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const imgConfig = (phase, { defaultConfig }) => {
+  if (phase === PHASE_EXPORT) {
+    return {
+      images: {
+        loader: "imgix",
+        path: "https://mogyuchi.imgix.net/",
+      },
+    };
+  }
+};
+
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
