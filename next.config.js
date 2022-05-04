bcdc6515f64f2c2205e4421f88e9d6ffc4982d18
Mogyuchi <git@mogyuchi.com>
2022-05-04 10:57:16 +0000

    config test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
-const { PHASE_PRODUCTION_BUILD,  PHASE_EXPORT} = require('next/constants')
+const { PHASE_EXPORT } = require("next/constants");
 
 module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_BUILD || phase === PHASE_EXPORT) {
+  if (process.env.CF_PAGES_BRANCH === "main") {
     return {
       reactStrictMode: true,
       images: {
         loader: "imgix",
         path: "https://mogyuchi.imgix.net/",
+      },
+    };
+  } else if (phase === PHASE_EXPORT) {
+    return {
+      reactStrictMode: true,
+      images: {
+        loader: "imgix",
+        path: "",
       },
     };
   } else return { reactStrictMode: true };
