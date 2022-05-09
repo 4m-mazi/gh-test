2214a2c071341c6557969ca7545aafc5bc971868
Mogyuchi <git@mogyuchi.com>
2022-05-09 21:35:19 +0900

    refactor image loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-const { PHASE_EXPORT, PHASE_PRODUCTION_BUILD } = require("next/constants");
 
-module.exports = (phase, { defaultConfig }) => {
+module.exports = () => {
   if (process.env.CF_PAGES_BRANCH === "main") {
     return {
       reactStrictMode: true,
@@ -10,7 +9,7 @@ module.exports = (phase, { defaultConfig }) => {
         path: "https://mogyuchi.imgix.net/",
       },
     };
-  } else if (process.env.CF_PAGES == 1 && (phase === PHASE_PRODUCTION_BUILD || phase === PHASE_EXPORT)) {
+  } else if (process.env.CF_PAGES == 1) {
     return {
       reactStrictMode: true,
       images: {
@@ -18,7 +17,7 @@ module.exports = (phase, { defaultConfig }) => {
       },
       env: {
         isPreview: true,
-      }
+      },
     };
   } else return { reactStrictMode: true };
 };
