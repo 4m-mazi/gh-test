9a74132d6b126edb9cd3bca189f9eeaf06eea68e
Mogyuchi <mogyuchi@mogyuchi.jp>
2023-07-07 02:15:58 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("Hello, world!");
-    if cfg!(target_os = "darwin") {
-        println!("You are running darwin!");
+    if cfg!(target_os = "macos") {
+        println!("You are running macos!");
     } else if cfg!(target_os = "linux") {
         println!("You are running linux!");
     } else if cfg!(target_os = "windows") {
