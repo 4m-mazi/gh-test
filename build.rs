33fb3b0397366f239cc26be2d420f79f2fb4217a
cm-ayf <cm.ayf2734@gmail.com>
2024-08-31 18:30:06 +0900

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,30 +1,23 @@
 use git2::{DescribeFormatOptions, DescribeOptions, Repository};
 
 fn main() {
-    let Ok(repo) = Repository::open(".").map_err(|e| {
-        println!("cargo::warning={}", e);
-        e
-    }) else {
-        return;
-    };
-    let Ok(git_describe_result) = repo
-        .describe(
-            DescribeOptions::new()
-                .describe_tags()
-                .show_commit_oid_as_fallback(true),
-        )
-        .and_then(|describe| {
-            describe.format(Some(
-                DescribeFormatOptions::new()
-                    .always_use_long_format(true)
-                    .dirty_suffix("-dirty"),
-            ))
-        }).map_err(|e| {
-            println!("cargo::warning={}", e);
-            e
-        })
-    else {
-        return;
-    };
-    println!("cargo::rustc-env=GIT_DESCRIBE={git_describe_result}")
+    let git_describe_result = match get_git_describe_result() {
+        Ok(result) => println!("cargo::rustc-env=GIT_DESCRIBE={result}"),
+        Err(e) => println!("cargo::warning={}", e),
+    }
+}
+
+fn get_git_describe_result() {
+    let repo = Repository::open(".")?;
+    let describe = repo.describe(
+        DescribeOptions::new()
+            .describe_tags()
+            .show_commit_oid_as_fallback(true),
+    )?;
+    let formatted = describe.format(Some(
+        DescribeFormatOptions::new()
+            .always_use_long_format(true)
+            .dirty_suffix("-dirty"),
+    ))?;
+    Ok(formatted)
 }
